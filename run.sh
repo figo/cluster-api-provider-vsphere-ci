b088ae63c7eec9988155f99d48fd22ebbdd48411
hui luo <luoh@vmware.com>
2018-10-18 14:25:20 -0700

    store the clusterctl binary only for temporary time

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-clusterctl create cluster --existing-bootstrap-cluster-kubeconfig ~/.kube/config -c ./spec/cluster.yaml -m ./spec/machines.yaml -p ./spec/provider-components.yaml --provider vsphere  -v 6
+./bin/clusterctl create cluster --existing-bootstrap-cluster-kubeconfig ~/.kube/config -c ./spec/cluster.yaml -m ./spec/machines.yaml -p ./spec/provider-components.yaml --provider vsphere  -v 6
