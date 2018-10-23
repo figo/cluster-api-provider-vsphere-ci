a225afb1108ae836a5dc166e065354d2a826fe78
hui luo <luoh@vmware.com>
2018-10-23 16:04:52 -0700

    remove all credentials, add support of reading secrets from enviroment variables

--- clusterctl.sh
+++ clusterctl.sh
@@ -1,3 +1,33 @@
 #!/bin/sh
+set -x
+# this script takes care of everything after bootstrap cluster created, it means
+# bootstrap need be created beforehand.
 
-~/go/bin/clusterctl create cluster --existing-bootstrap-cluster-kubeconfig ~/.kube/config -c ./spec/cluster.yaml -m ./spec/machines.yaml -p ./spec/provider-components.yaml --provider vsphere  -v 6
+# update specs, requires following enviroments variables:
+# POD_CIDR
+# VSPHERE_SERVER
+# VSPHERE_USERNAME
+# VSPHERE_PASSWORD
+# VSPHERE_MACHINE_CONTROLLER_REGISTRY
+# TARGET_VM_SSH  (base64 encoded)
+# TARGET_VM_SSH_PUB (base64 encoded)
+
+for filename in spec/*.template; do
+  newfilename="$(echo "$filename" | sed 's/template/yml/g')"
+  rm -f "$newfilename" temp.sh  
+  ( echo "cat <<EOF >$newfilename";
+    cat "$filename";
+    echo "EOF";
+  ) >temp.sh
+  chmod +x temp.sh
+  ./temp.sh
+done
+rm temp.sh
+
+
+# run clusterctl
+./bin/clusterctl create cluster --existing-bootstrap-cluster-kubeconfig ~/.kube/config -c ./spec/cluster.yml -m ./spec/machines.yml -p ./spec/provider-components.yml --provider vsphere  -v 6
+
+
+# cleanup the cluster
+# TODO (clusterctl delete is not working, but does not support existing bootstrap cluster)
