6633c752bdbd24debb28e0ba5b18aebe6520b2b7
hui luo <luoh@vmware.com>
2018-10-25 15:55:07 -0700

    add travis-ci container

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-the ci to run cluster-api-provider-vsphere
+Test cluster-api-provider-vsphere
+
+***Launch CI***
+```
+docker run \
+  --rm \
+  -v $HOME/.ssh:/root/ssh \
+  -e GOVC_URL=$GOVC_URL \
+  -e GOVC_USERNAME=$GOVC_USERNAME \
+  -e GOVC_PASSWORD=$GOVC_PASSWORD \
+  -e JUMPHOST=$JUMPHOST \
+  -e GOVC_INSECURE="true" \
+  -e VSPHERE_MACHINE_CONTROLLER_REGISTRY=$VSPHERE_MACHINE_CONTROLLER_REGISTRY \
+  -ti luoh/cluster-api-provider-vsphere-travis-ci:latest
+```
+
+note: set `$VSPHER_MACHINE_CONTROLLER_REGISTRY` if you want to test your local build controller
---
+++ travis-ci/travis-ci.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+# it requires the following enviroment variable:
+# JUMPHOST
+# GOVC_URL
+# GOVC_USERNAME
+# GOVC_PASSWORD
+# VSPHERE_MACHINE_CONTROLLER_REGISTRY
+
+# and it requires container has volumes
+# /root/ssh/jumphost
+# /root/ssh/bootstrapper
+
+bootstrap_vm_ip=""
+get_bootstrap_vm() {
+   dc=$(govc find -type d)
+   bootstrap_vm=$(govc find vm -name clusterapi-bootstrap)
+   bootstrap_vm_name="${dc}/${bootstrap_vm}"
+   snapshot_name="cluster-api-provider-vsphere-ci-0.0.1"
+   govc snapshot.revert -vm "${bootstrap_vm_name}" "${snapshot_name}"  
+   bootstrap_vm_ip=$(govc vm.ip "${bootstrap_vm_name}")
+}
+
+run_cmd_on_bootstrap() {
+   ssh -o ProxyCommand="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /root/ssh/jumphost -W %h:%p luoh@$JUMPHOST" \
+       vmware@"$1" -i /root/ssh/bootstrapper -o "StrictHostKeyChecking=no" -o "UserKnownHostsFile=/dev/null" "$2"
+}
+
+delete_vm() {
+   dc=$(govc find -type d)
+   vm=$(govc find vm -name "$1""-*")
+   govc vm.power -off "${dc}/${vm}"
+   govc vm.destroy "${dc}/${vm}"
+}
+
+get_bootstrap_vm
+
+controller_registry=${VSPHERE_MACHINE_CONTROLLER_REGISTRY:-luoh/cluster-api-provider-vsphere:0.0.11}
+echo "test ${controller_registry}"
+run_cmd_on_bootstrap "${bootstrap_vm_ip}" "sed -i 's|{VSPHERE_MACHINE_CONTROLLER_REGISTRY}|'${controller_registry}'|g' ~/.config/envs"
+
+# ssh to bootstrap VM and deploy the CI container to bootstrap cluster
+job="kubectl create -f https://gist.githubusercontent.com/figo/989ede156d4a0d722244fb0c16d5ba80/raw/3a995366a08e361d0ca8d9892a82b580eda4b91b/job.yml"
+run_cmd_on_bootstrap "${bootstrap_vm_ip}" "${job}"
+
+# wait for job to finish
+run_cmd_on_bootstrap "${bootstrap_vm_ip}" "~/lib/wait_for_job_finished.sh 600"
+ret="$?"
+
+# cleanup
+delete_vm "clusterapi-master"
+get_bootstrap_vm
+
+exit "${ret}"
